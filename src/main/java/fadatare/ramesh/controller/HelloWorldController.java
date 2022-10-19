aa325482b9d83dd62e182d9c8baba1829747c014
chigov2 <chigov@gmail.com>
2022-10-19 16:48:12 +0300

    17

--- src/main/java/fadatare/ramesh/controller/HelloWorldController.java
+++ src/main/java/fadatare/ramesh/controller/HelloWorldController.java
@@ -1,4 +1,9 @@
 package fadatare.ramesh.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
 public class HelloWorldController {
 }
